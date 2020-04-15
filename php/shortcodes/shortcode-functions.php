52edb774a22b37cc0aee666999176becfce33d98
Berkeley High Jacket <web@bhsjacket.com>
2020-04-15 10:46:07 -0700

    Hide all default shortcodes

--- php/shortcodes/shortcode-functions.php
+++ php/shortcodes/shortcode-functions.php
@@ -59,85 +59,35 @@ function shortcode_styles() {
 }
 // End Shortcode Editor Styles
 
-
 // Remove Shortcodes
-add_filter( 'su/data/shortcodes', 'remove_su_shortcodes' );
+add_filter(
+  'su/data/shortcodes',
+  function($shortcodes) {
 
-function remove_su_shortcodes( $shortcodes ) {
+      return array_filter(
+          $shortcodes,
+          function($shortcode) {
+              return isset($shortcode['group']) && 'shortcode-creator' === $shortcode['group'];
+          }
+      );
 
-  unset( $shortcodes['button'] );
-  unset( $shortcodes['heading'] );
-  unset( $shortcodes['tabs'] );
-  unset( $shortcodes['tab'] );
-  unset( $shortcodes['spoiler'] );
-  unset( $shortcodes['accordion'] );
-  unset( $shortcodes['divider'] );
-  unset( $shortcodes['spacer'] );
-  unset( $shortcodes['highlight'] );
-  unset( $shortcodes['label'] );
-  unset( $shortcodes['quote'] );
-  unset( $shortcodes['pullquote'] );
-  unset( $shortcodes['dropcap'] );
-  unset( $shortcodes['row'] );
-  unset( $shortcodes['column'] );
-  unset( $shortcodes['list'] );
-  unset( $shortcodes['service'] );
-  unset( $shortcodes['box'] );
-  unset( $shortcodes['note'] );
-  unset( $shortcodes['expand'] );
-  unset( $shortcodes['lightbox'] );
-  unset( $shortcodes['lightbox_content'] );
-  unset( $shortcodes['tooltip'] );
-  unset( $shortcodes['private'] );
-  unset( $shortcodes['youtube'] );
-  unset( $shortcodes['youtube_advanced'] );
-  unset( $shortcodes['vimeo'] );
-  unset( $shortcodes['dailymotion'] );
-  unset( $shortcodes['audio'] );
-  unset( $shortcodes['video'] );
-  unset( $shortcodes['table'] );
-  unset( $shortcodes['csv_table'] );
-  unset( $shortcodes['permalink'] );
-  unset( $shortcodes['members'] );
-  unset( $shortcodes['guests'] );
-  unset( $shortcodes['feed'] );
-  unset( $shortcodes['menu'] );
-  unset( $shortcodes['subpages'] );
-  unset( $shortcodes['siblings'] );
-  unset( $shortcodes['map'] );
-  unset( $shortcodes['image_carousel'] );
-  unset( $shortcodes['slider'] );
-  unset( $shortcodes['custom_gallery'] );
-  unset( $shortcodes['posts'] );
-  unset( $shortcodes['dummy_text'] );
-  unset( $shortcodes['dummy_image'] );
-  unset( $shortcodes['animate'] );
-  unset( $shortcodes['meta'] );
-  unset( $shortcodes['user'] );
-  unset( $shortcodes['post'] );
-  unset( $shortcodes['template'] );
-  unset( $shortcodes['arcade'] );
-  unset( $shortcodes['scheduler'] );
-  unset( $shortcodes['splash'] );
-  unset( $shortcodes['exit_popup'] );
-  unset( $shortcodes['panel'] );
-  unset( $shortcodes['photo_panel'] );
-  unset( $shortcodes['icon_panel'] );
-  unset( $shortcodes['icon_text'] );
-  unset( $shortcodes['progress_pie'] );
-  unset( $shortcodes['progress_bar'] );
-  unset( $shortcodes['member'] );
-  unset( $shortcodes['section'] );
-  unset( $shortcodes['pricing_table'] );
-  unset( $shortcodes['testimonial'] );
-  unset( $shortcodes['icon'] );
-  unset( $shortcodes['content_slider'] );
-  unset( $shortcodes['shadow'] );
-  unset( $shortcodes['gmap'] );
-  unset( $shortcodes['carousel'] );
-  unset( $shortcodes['qrcode'] );
+  },
+  99,
+  1
+);
 
-  return $shortcodes;
+add_filter(
+  'su/data/groups',
+  function($groups) {
 
-}
+      return array_filter(
+          $groups,
+          function($group) {
+              return in_array($group, array('all'));
+          },
+          ARRAY_FILTER_USE_KEY
+      );
+
+  }
+);
 // End Remove Shortcodes
