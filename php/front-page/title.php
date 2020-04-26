e419fa08240000a88184be50172c2e4982c82334
Jerome Paulos <jeromepaulos@yahoo.com>
2020-04-26 00:18:05 -0700

    Center title

--- php/front-page/title.php
+++ php/front-page/title.php
@@ -3,6 +3,8 @@ $display = get_query_var('display-info');
 ?>
 
 <section class="title">
-    <i style="color: <?php echo $display['icon-color']; ?>" class="<?php echo $display['icon']; ?>"></i>
-    <h2><a style="color: <?php echo $display['text-color']; ?>" href="<?php echo $display['url']; ?>"><?php echo $display['title']; ?></a></h2>
+    <div class="title-inner" style="margin: 0 auto; display: flex;">
+        <i style="color: <?php echo $display['icon-color']; ?>" class="<?php echo $display['icon']; ?>"></i>
+        <h2><a style="color: <?php echo $display['text-color']; ?>" href="<?php echo $display['url']; ?>"><?php echo $display['title']; ?></a></h2>
+    </div>
 </section>
