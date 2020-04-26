c8358b9c1e01566fe4eb6fe97787e4d0660acef7
Jerome Paulos <jeromepaulos@yahoo.com>
2020-04-26 01:50:25 -0700

    COmmit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
   jQuery(".article-content > p:last-child").append(" &#x25C6;"); // Add Tombstone
   $('#advanced-search-popup').hide(); // Advanced Search
   mediumZoom('.article-content img'); // Image Zoom
-  mediumZoom('#page img'); // Image Zoom
+  mediumZoom('.image-in-post > img'); // Image Zoom
   mediumZoom('.featured-image > img'); // Image Zoom
 });
 
